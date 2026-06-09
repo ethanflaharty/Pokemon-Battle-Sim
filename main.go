d5d545b11f32133dc90f3aa364479a46b0e7ac52
Ethan Flaharty <ethanflahartybusiness@gmail.com>
2026-06-08 19:21:32 -0500

    5. cleaned up battle logic as well as adding damage ranges and crits

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	}
 	wild.HP = pokemondata.HPForumla(wild)
 	wild.CalculateStats()
-	trainerPokemon := pokemondata.Pokemon{
+	ally := pokemondata.Pokemon{
 		Name: "Bulbasaur",
 		BaseStats: pokemondata.PokemonBaseStats{
 			HP:        45,
@@ -32,41 +32,9 @@ func main() {
 		},
 		Level: 50,
 	}
-	trainerPokemon.HP = pokemondata.HPForumla(trainerPokemon)
-	trainerPokemon.CalculateStats()
+	ally.HP = pokemondata.HPForumla(ally)
+	ally.CalculateStats()
 	fmt.Printf("A wild %v appeared!\n", wild.Name)
-	fmt.Printf("Go! %v!\n", trainerPokemon.Name)
-	for trainerPokemon.HP != 0 && wild.HP != 0 {
-		if wild.Speed > trainerPokemon.Speed {
-			trainerPokemon.HP = trainerPokemon.HP - pokemondata.Damage(wild, trainerPokemon)
-			if trainerPokemon.HP <= 0 {
-				trainerPokemon.HP = 0
-				fmt.Printf("%v has fainted!\n", trainerPokemon.Name)
-			} else if trainerPokemon.HP > 0 {
-				fmt.Printf("%v now has %v HP!\n", trainerPokemon.Name, trainerPokemon.HP)
-			}
-			wild.HP = wild.HP - pokemondata.Damage(trainerPokemon, wild)
-			if wild.HP <= 0 {
-				wild.HP = 0
-				fmt.Printf("%v has fainted!\n", wild.Name)
-			} else if wild.HP > 0 {
-				fmt.Printf("%v now has %v HP!\n", wild.Name, wild.HP)
-			}
-		} else {
-			wild.HP = wild.HP - pokemondata.Damage(trainerPokemon, wild)
-			if wild.HP <= 0 {
-				wild.HP = 0
-				fmt.Printf("%v has fainted!\n", wild.Name)
-			} else if wild.HP > 0 {
-				fmt.Printf("%v now has %v HP!\n", wild.Name, wild.HP)
-			}
-			trainerPokemon.HP = trainerPokemon.HP - pokemondata.Damage(wild, trainerPokemon)
-			if trainerPokemon.HP <= 0 {
-				trainerPokemon.HP = 0
-				fmt.Printf("%v has fainted!\n", trainerPokemon.Name)
-			} else if trainerPokemon.HP > 0 {
-				fmt.Printf("%v now has %v HP!\n", trainerPokemon.Name, trainerPokemon.HP)
-			}
-		}
-	}
+	fmt.Printf("Go! %v!\n", ally.Name)
+	pokemondata.BattleSequence(&ally, &wild)
 }
--- pokemonData/battle.go
+++ pokemonData/battle.go
@@ -1,5 +1,81 @@
 package pokemondata
 
-func Damage(attacker, defender Pokemon) int {
-	return (((((2 * attacker.Level) / 5) + 2) * 50 * attacker.Attack / defender.Defense) / 50) + 2
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+type DamageResult struct {
+	Damage int
+	Crit   bool
+}
+
+func Damage(attacker, defender Pokemon) DamageResult {
+	damage := ((((((2 * attacker.Level) / 5) + 2) * 50 * attacker.Attack / defender.Defense) / 50) + 2)
+
+	multiplier := float64(rand.IntN(16)+85) / 100.0
+
+	result := DamageResult{}
+
+	if rand.Float64() < 0.0625 {
+		result.Crit = true
+		result.Damage = int(float64(damage) * multiplier * 1.5)
+	} else {
+		result.Damage = int(float64(damage) * multiplier)
+	}
+	return result
+}
+
+func ApplyDamage(attacker, defender *Pokemon) {
+	damageTaken := Damage(*attacker, *defender)
+	defender.HP -= damageTaken.Damage
+	if damageTaken.Crit {
+		fmt.Print("A critical hit! ")
+	}
+	if defender.HP <= 0 {
+		defender.HP = 0
+		fmt.Printf("%v took %v damage! ", defender.Name, damageTaken.Damage)
+		fmt.Printf("%v has fainted!\n", defender.Name)
+
+	} else {
+		fmt.Printf("%v took %v damage! ", defender.Name, damageTaken.Damage)
+		fmt.Printf("%v now has %v HP!\n", defender.Name, defender.HP)
+	}
+}
+
+func BattleSequence(ally, foe *Pokemon) {
+	turn := 0
+	for ally.HP > 0 && foe.HP > 0 {
+		turn += 1
+		fmt.Println()
+		fmt.Printf("Turn %v\n", turn)
+		fmt.Println()
+		if ally.Speed > foe.Speed {
+			ApplyDamage(ally, foe)
+
+			if foe.HP > 0 {
+				ApplyDamage(foe, ally)
+			}
+		} else if foe.Speed > ally.Speed {
+			ApplyDamage(foe, ally)
+
+			if ally.HP > 0 {
+				ApplyDamage(ally, foe)
+			}
+		} else {
+			if rand.IntN(2) == 0 {
+				ApplyDamage(ally, foe)
+
+				if foe.HP > 0 {
+					ApplyDamage(foe, ally)
+				}
+			} else {
+				ApplyDamage(foe, ally)
+
+				if ally.HP > 0 {
+					ApplyDamage(ally, foe)
+				}
+			}
+		}
+	}
 }
